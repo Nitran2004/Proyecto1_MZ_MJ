4a1c757fead260ff9b3b439436f9f0df6b0015a6
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-11-02 15:53:12 -0500

    2 commit, segunda tabla

--- Models/Comentario.cs
+++ Models/Comentario.cs
@@ -2,7 +2,15 @@
 {
     public class Comentario
     {
-
+        public int ComentarioId { get; set; }
+        public string? Texto { get; set; }
+        // Otros atributos relacionados con los comentarios
+        public string HabitacionTipo
+        {
+            get { return Habitacion.Tipo; }
+        }
+        public int HabitacionId { get; set; }
+        public Habitacion? Habitacion { get; set; }
 
     }
 }
