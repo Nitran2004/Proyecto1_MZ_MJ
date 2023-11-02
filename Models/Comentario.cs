3795144527b9854ac2311eb68d8c99e6f9740056
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-11-02 18:11:35 -0500

    7 commit Correcion segunda tabla en Views

--- Models/Comentario.cs
+++ Models/Comentario.cs
@@ -5,10 +5,10 @@
         public int ComentarioId { get; set; }
         public string? Texto { get; set; }
         // Otros atributos relacionados con los comentarios
-        public string HabitacionTipo
-        {
-            get { return Habitacion.Tipo; }
-        }
+        //public string HabitacionTipo
+        //{
+          //  get { return Habitacion.Tipo; }
+        //}
         public int HabitacionId { get; set; }
         public Habitacion? Habitacion { get; set; }
 
