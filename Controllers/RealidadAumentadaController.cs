1bd628fb3fde219a2e23f7c033677d8fe9341df3
Aaron Zumarraga <aaron.zumarraga@hotmail.com>
2025-05-11 15:55:32 -0500

    Funcionalidad base de realidad aumentada

---
+++ Controllers/RealidadAumentadaController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Proyecto1_MZ_MJ.Data;
+using Proyecto1_MZ_MJ.Models;
+using System.Threading.Tasks;
+
+namespace Proyecto1_MZ_MJ.Controllers
+{
+    public class RealidadAumentadaController : Controller
+    {
+        private readonly Proyecto1_MZ_MJContext _context;
+
+        public RealidadAumentadaController(Proyecto1_MZ_MJContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> VistaAR(int id)
+        {
+            // Obtener el producto para pasar sus datos a la vista
+            var producto = await _context.Productos.FindAsync(id);
+            if (producto == null)
+            {
+                return NotFound();
+            }
+
+            return View(producto);
+        }
+    }
+}
